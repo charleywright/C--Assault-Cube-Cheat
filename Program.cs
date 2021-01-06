ea762a02021cac8b0c9c03563ff6106c3e596648
Charley Wright <charleywright06@gmail.com>
2021-01-06 22:51:49 +0000

    Console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Assault_Cube_mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
