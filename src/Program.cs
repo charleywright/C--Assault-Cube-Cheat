b5ba3639c31e176423290fbe451298ce0f95d6fd
Charley Wright <charleywright06@gmail.com>
2021-01-13 10:57:06 +0000

    Reversal

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C__Assault_Cube_mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assault Cube cheat - Made by Charley Wright / Joder#9009");
+            Console.Title = "Assault Cube cheat - Made by Charley Wright / Joder#9009";
+
+
+        }
+    }
+}
